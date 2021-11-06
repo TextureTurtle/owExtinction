ea585ad5659b037181cdbe168c34b6c34da72786
TextureTurtle <mraidanhall@gmail.com>
2021-11-06 17:38:12 -0400

    Updated Splash Screen

--- ModTemplate/ModTemplate.cs
+++ ModTemplate/ModTemplate.cs
@@ -22,19 +22,18 @@ namespace ModTemplate
         }
         private static string dllPath;
         public static string DllExecutablePath { get { if (string.IsNullOrEmpty(dllPath)) dllPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); return dllPath; } private set { } }
-        private void Start()
+        void OnCompleteSceneLoad(OWScene originalScene, OWScene loadScene)
         {
-            // Starting here, you'll have access to OWML's mod helper.
-
             // Import Stuff
             // ==============================
+            ModHelper.Console.WriteLine($"SUS", MessageType.Info);
 
             // Audio
 
             // Images
-            Texture2D newSplash = ImportTextureFromImage(Path.Combine(DllExecutablePath, "GamepadSplashImage.png"));
-            Texture2D oweLogo = ImportTextureFromImage(Path.Combine(DllExecutablePath, "MENU_OWE_d.png"));
-            var duckAsset = ModHelper.Assets.LoadTexture("GamepadSplashImage.png");
+
+            //Texture2D oweLogo = ImportTextureFromImage(Path.Combine(DllExecutablePath, "MENU_OWE_d.png"));
+
             // Models
 
             // Replace or add Assets
@@ -42,16 +41,29 @@ namespace ModTemplate
 
             // Audio
 
-            var ogTheme = Resources.FindObjectsOfTypeAll<AudioSource>().First(x => x.name == "AudioSource_Music");
-            ogTheme = ogTheme.GetComponent<AudioSource>();
-            ogTheme.clip = (AudioClip)Resources.Load("Main.mp3", typeof(AudioClip));
+            //var ogTheme = Resources.FindObjectsOfTypeAll<AudioSource>().First(x => x.name == "AudioSource_Music");
+            //ogTheme = ogTheme.GetComponent<AudioSource>();
+            //ogTheme.clip = (AudioClip)Resources.Load("Main.mp3", typeof(AudioClip));
 
             // Images
-            var oldSplash = Resources.FindObjectsOfTypeAll<Image>().First(x => x.name == "GamepadSplashImage");
-            oldSplash.material.mainTexture = newSplash;
+
 
             // Models
 
+            ModHelper.Console.WriteLine($"SUS2", MessageType.Info);
+        }
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+
+            // Menu Stuff
+            Sprite newSplash = LoadNewSprite(Path.Combine(DllExecutablePath, "GamepadSplashImage.png"));
+            var oldSplash = Resources.FindObjectsOfTypeAll<Image>().First(x => x.name == "GamepadSplashImage");
+            oldSplash.sprite = newSplash;
+
+
+            // End Stuff
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
             ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
         }
         public static Texture2D ImportTextureFromImage(string filePath)
@@ -60,6 +72,14 @@ namespace ModTemplate
             Texture2D image = new Texture2D(2, 2); image.LoadImage(imageBytes);
             return image;
         }
- 
+        public Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f, SpriteMeshType spriteType = SpriteMeshType.Tight)
+        {
+            // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
+            Texture2D SpriteTexture = ImportTextureFromImage(FilePath);
+            Sprite NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit, 0, spriteType);
+
+            return NewSprite;
+        }
+
     }
 }
